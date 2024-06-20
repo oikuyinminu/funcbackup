9bd6b8d71ec9325fe6722e228b9b682384a6c29b
Banji IKUYINMINU <i.banji@hotmail.com>
2024-06-20 14:57:36 +1000

    create the snapshot processing typscript

--- src/functions/PerformBlobSnapShot.ts
+++ src/functions/PerformBlobSnapShot.ts
@@ -1,14 +1,14 @@
 import { app, InvocationContext, Timer } from "@azure/functions";
 import * as blobService from "../services/StorageProcessor.js";
 
-export async function SetBlobSnapshot(myTimer: Timer, context: InvocationContext): Promise<void> {
+export async function PerformBlobSnapshots(myTimer: Timer, context: InvocationContext): Promise<void> {
   context.log('Timer function processed request.');
 
   // Process storage account snapshots
   await blobService.processStorageAccountSnapshots(context);
 }
 
-app.timer('SetBlobSnapshot', {
+app.timer('PerformBlobSnapshots', {
   schedule: '*/5 * * * *', // To be adjusted as needed
-  handler: SetBlobSnapshot,
+  handler: PerformBlobSnapshots
 });
